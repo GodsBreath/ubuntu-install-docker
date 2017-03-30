283914c5d92a92526bf693f32e6d4b98d4fe1bc6
Daniel Arant <snoop.daniels@gmail.com>
2017-03-29 21:51:17 -0400

    Initialize repository

---
+++ install-docker.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+echo Installing Docker
+
+read -p "Production install? (y/N): " production
+
+if [[ "$production" == "y" ]]
+	apt-cache madison docker-ce
+	read -p "Version (latest): " version
+fi
+
+echo Removing Legacy Version
+
+sudo apt-get remove docker docker-engine
+
+echo Installing Prerequisites
+
+sudo apt-get install \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    software-properties-common 
+
+echo Downloadking Repository Key
+
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+
+echo Setting Up Repository
+
+sudo add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   $(lsb_release -cs) \
+   stable"
+
+echo Updating Package Index
+
+sudo apt-get update
+
+echo Installing Docker Community Edition
+
+if [[ "$production" == "y" ]]
+	sudo apt-get install docker-ce=$version
+else
+	sudo apt-get install docker-ce
+fi
+
+echo Running Test Container
+
+sudo docker run hello-world
---
+++ readme.md
@@ -0,0 +1,3 @@
+Docker Installation Script for Ubuntu v0.0.1
+
+Based on instructions found at [https://docs.docker.com/engine/installation/linux/ubuntu/#install-using-the-repository](https://docs.docker.com/engine/installation/linux/ubuntu/#install-using-the-repository) as of 3/29/2017
